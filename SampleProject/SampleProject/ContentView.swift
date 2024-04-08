341fb405789e3c02be4812efa8b284fedace901d
emrepun <30630955+emrepun@users.noreply.github.com>
2024-04-08 17:32:17 +0200

    Add base sample project

---
+++ SampleProject/SampleProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleProject
+//
+//  Created by Emre Havan on 08.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleProject/SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by Emre Havan on 08.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
