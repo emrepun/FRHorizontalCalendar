2f84d20b99fafa6767ffe6233b06703f136463b1
emrepun <30630955+emrepun@users.noreply.github.com>
2025-04-10 17:10:34 +0200

    Add test for setHeight method

--- Tests/FRHorizontalCalendarTests/FRHorizontalCalendarViewModelTests.swift
+++ Tests/FRHorizontalCalendarTests/FRHorizontalCalendarViewModelTests.swift
@@ -26,6 +26,21 @@ final class FRHorizontalCalendarViewModelTests: XCTestCase {
         super.tearDown()
     }
 
+    // MARK: - setHeight(_ height: CGFloat) tests
+    func test_delegate_is_notified_only_once_when_height_is_set() throws {
+        let sut = FRHorizontalCalendarViewModel(startDate: calendarStartDate)
+        sut.delegate = delegateMock
+        let expectation = XCTestExpectation()
+        expectation.assertForOverFulfill = true
+        delegateMock.expectationToFullfilWhenDidSetInitialHeightIsCalled = expectation
+
+        // Call it twice to ensure it won't fulfill the expectation twice
+        sut.setHeight(100.0)
+        sut.setHeight(100.0)
+
+        wait(for: [expectation], timeout: 0.1)
+    }
+
     private func subtractDays(from startDate: Date, dayCount: Int) -> Date? {
         guard let newDate = Calendar.current.date(byAdding: .day, value: -dayCount, to: startDate) else {
             fatalError()
