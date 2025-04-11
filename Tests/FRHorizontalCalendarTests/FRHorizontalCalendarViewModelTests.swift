c94bf618b2dc28621c53991f529c1f1b392eb057
emrepun <30630955+emrepun@users.noreply.github.com>
2025-04-11 09:47:29 +0200

    Add dayStringForegroundColorFor tests

--- Tests/FRHorizontalCalendarTests/FRHorizontalCalendarViewModelTests.swift
+++ Tests/FRHorizontalCalendarTests/FRHorizontalCalendarViewModelTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import FRHorizontalCalendar
+import SwiftUI
 
 final class FRHorizontalCalendarViewModelTests: XCTestCase {
 
@@ -62,8 +63,53 @@ final class FRHorizontalCalendarViewModelTests: XCTestCase {
         XCTAssertEqual(result, "10")
     }
 
+    // MARK: - dayStringForegroundColorFor(_ day: FRCalendarDayModel) -> Color
+
+    func test_correct_day_string_foreground_color_is_provided_for_a_non_available_day() {
+        let sut = makeSUTAndSetDelegate()
+        let day = makeDay()
+
+        let result = sut.dayStringForegroundColorFor(day)
+
+        XCTAssertEqual(result, Color(uiColor: .systemGray5))
+    }
+
+    func test_correct_day_string_foreground_color_is_provided_for_an_available_and_selected_day() {
+        let sut = makeSUTAndSetDelegate()
+        let day = makeDay(isSelected: true, isAvailable: true)
+
+        let result = sut.dayStringForegroundColorFor(day)
+
+        XCTAssertEqual(result, Color.white)
+    }
+
+    func test_correct_day_string_color_is_provided_for_an_available_unselected_day_when_it_is_today() {
+        let sut = makeSUTAndSetDelegate()
+        let beginningOfToday = Calendar.current.startOfDay(for: .now)
+        let day = makeDay(date: beginningOfToday, isAvailable: true)
+
+        let result = sut.dayStringForegroundColorFor(day)
+
+        XCTAssertEqual(result, Color.blue)
+    }
+
+    func test_correct_day_string_color_is_provided_for_an_available_unselected_previous_day() throws {
+        let sut = makeSUTAndSetDelegate()
+        // This date will guaranteed to be non-today for any day this unit tests are run in the future
+        let dateComponents = DateComponents(year: 2025, month: 4, day: 9)
+        let date = try XCTUnwrap(Calendar.current.date(from: dateComponents))
+        let day = makeDay(date: date, isAvailable: true)
+
+        let result = sut.dayStringForegroundColorFor(day)
+
+        XCTAssertEqual(result, Color(uiColor: .systemGray))
+    }
+    
     // MARK: - Utilities
 
+    private func makeDay(date: Date = .now, isSelected: Bool = false, hasContentAvailable: Bool = false, isAvailable: Bool = false) -> FRCalendarDayModel {
+        FRCalendarDayModel(date: date, isSelected: isSelected, hasContentAvailable: hasContentAvailable, isAvailable: isAvailable)
+    }
     private func makeDateForARegularDay() -> Date? {
         let dateComponents = DateComponents(year: 2025, month: 4, day: 10)
         return Calendar.current.date(from: dateComponents)
